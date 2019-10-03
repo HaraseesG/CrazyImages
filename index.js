57c9a17f751c295c37ff9dde5620d72317ed7551
HaraseesG <45112294+HaraseesG@users.noreply.github.com>
2019-10-02 23:17:10 -0400

    Uploaded personal project

---
+++ index.js
@@ -0,0 +1,23 @@
+const{BrowserWindow, app} = require('electron')
+
+let win;
+
+let boot = () => {
+	win = new BrowserWindow({
+		width:700,
+		height: 500,
+		frame: false,
+		webPreferences: {
+			nodeIntegration: true
+		}
+	});
+
+	win.loadURL(`file://${__dirname}/index.html`);
+
+	win.webContents.openDevTools();
+
+};
+
+
+
+app.on('ready', boot);
